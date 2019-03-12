64b1d13b22c7fea5af24cdb3edbba04188c05d66
Revzan Kamarullah <revzakamarullah@gmail.com>
2019-03-12 18:45:49 +0700

    Add fetch on app init

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
-import AddTodoForm from './containers/AddTodoForm';
+import { connect } from 'react-redux';
 import TodoFilter from './containers/TodoFilter';
+import AddTodoForm from './containers/AddTodoForm';
 import VisibleTodoList from './containers/VisibleTodoList';
+import { fetchTodo } from './actions';
 
 const AppWrapper = styled.div`
   width:360px;
   margin: 0 auto;
 `
 
-class App extends Component {
-  render() {
-    return (
-      <AppWrapper>
+let App = () => {
+  return (
+    <AppWrapper>
       <h1>Todo App</h1>
 
       <AddTodoForm/>
       <TodoFilter/>
       <VisibleTodoList/>
     </AppWrapper>
-    );
-  }
+  );
 }
+
+const mapDispatchToProps = dispatch => ({
+  fetchTodos: dispatch(fetchTodo())
+})
+
+App = connect(null, mapDispatchToProps)(App)
 
 export default App;
